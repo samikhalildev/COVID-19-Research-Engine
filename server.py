bb438a8b5e63d775d7d1cce541644ffdaf7aad23
samikhalildev <sami.khalil.dev@gmail.com>
2020-04-02 23:11:24 +1100

    Add python code

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask,request,jsonify,render_template
+from utils import cleanText, search_articles
+from flask_cors import CORS, cross_origin
+import json
+
+articles = json.load(open('./data/data_structure.json', 'rb'))
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/")
+def hello():
+    return 'test API'
+
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('query')
+    query = cleanText(query)
+    query = query.split(',')
+    print(query)
+    res = search_articles(articles, query)
+    return jsonify(res)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,67 @@
+import json
+
+def getText(x):
+    text = []
+    for t in x:
+        text.append(t['text'])
+        
+    text = ''.join(text)
+    return text
+
+def getSentences(text):
+    sentences = []
+    if len(text) > 0:
+        if '. ' in text:
+            for sentence in text.split('. '):
+                sentence = sentence.strip()
+                if len(sentence) > 3:
+                    sentences.append(sentence)
+        else:
+            if len(text) > 3:
+                sentences.append(text)
+    return sentences
+
+def lastElement(i, arr):
+    return i == len(arr) - 1
+
+def cleanText(text):
+    text = text.strip().lower()
+    return text
+
+def search_articles(articles, query):
+    searchIndex = {}
+
+    ''' an array of objects
+    Structure = [
+            'paper_id',
+            'title',
+            'authors',
+            'abstract',
+            'body'
+        ]
+    '''
+
+    for article in articles[:4]:
+        counter = 0
+
+        while counter != 2:
+            sectionType = 'abstract' if counter == 0 else 'body'
+            for i in range(len(article[sectionType])):
+                paragraph = article[sectionType][i]
+
+                section = paragraph['section']
+                text = paragraph['text']
+                
+                sentences = text.split('. ')
+                for j in range(len(sentences)):
+                    sentence = sentences[j]
+                    for term in query:
+                        if term in sentence.lower():
+                            ob = {'sentence': sentence, 'article': article, 'sectionType': sectionType, 'paraIndex': i, 'sentIndex': j}
+                            if term in searchIndex:
+                                searchIndex[term].append(ob)
+                            else:
+                                searchIndex[term] = [ob]
+            counter += 1
+                        
+    return searchIndex
