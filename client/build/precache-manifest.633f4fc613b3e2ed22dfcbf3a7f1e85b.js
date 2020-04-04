f8165d29914e31c8345839a30f6db6064cf6ffba
samikhalildev <sami.khalil.dev@gmail.com>
2020-04-04 19:20:24 +1100

    react build

---
+++ client/build/precache-manifest.633f4fc613b3e2ed22dfcbf3a7f1e85b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1256b640a0161d4ca880373cffba78be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5b30fd6f30eccc895c0",
+    "url": "/static/css/main.9a0e9588.chunk.css"
+  },
+  {
+    "revision": "399c5bdad7b951431968",
+    "url": "/static/js/2.ac38f60a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ac38f60a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5b30fd6f30eccc895c0",
+    "url": "/static/js/main.2dcdc8e0.chunk.js"
+  },
+  {
+    "revision": "40c04b19a92011560076",
+    "url": "/static/js/runtime-main.3e95f401.js"
+  }
+]);
